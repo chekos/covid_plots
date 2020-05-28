e305966dc1379c0a40b9cac7f14f7b0705bfbbf1
Sergio Sanchez <ssanchez@alluma.org>
2020-05-28 13:56:26 -0700

    github secrets en scripts cchale

--- .github/utils/kaggle_credentials.sh
+++ .github/utils/kaggle_credentials.sh
@@ -1,4 +1,4 @@
 # make .kaggle dir and write kaggle.json
 mkdir ~/.kaggle 
 
-echo "{\"username\":\"tacosdedatos\",\"key\":\"${{ secrets.KAGGLE_API_KEY }}\"}" > ~/.kaggle/kaggle.json
+echo "{\"username\":\"tacosdedatos\",\"key\":\"${KAGGLE_API_KEY}\"}" > ~/.kaggle/kaggle.json
